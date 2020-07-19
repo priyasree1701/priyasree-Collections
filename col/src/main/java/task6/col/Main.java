52efc8601a67c86d70720ee2173158447cb95985
priyasree1701 <priyasree.manthri@gmail.com>
2020-07-20 01:31:52 +0530

    task 6

---
+++ col/src/main/java/task6/col/Main.java
@@ -0,0 +1,20 @@
+package task6.col;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CustomList<Integer> l=new CustomList<Integer>();
+		l.add(21);
+		l.add(22);
+		l.add(23);
+		l.display();
+		System.out.println("getting first element "+l.get(0));
+		System.out.println("Size is "+ l.size());
+		System.out.println("removing 3rd element "+l.remove(2));
+		l.display();
+		
+		
+	}
+
+}
